f2a693ed93f1865a7701ed60280b4f2ff36ab5f6
outfrost <kotlet.bahn@gmail.com>
2020-07-25 10:49:13 +0200

    Make charge indicators on robot actually work

--- functional/Robot.gd
+++ functional/Robot.gd
@@ -2,16 +2,39 @@ extends RigidBody
 
 const util = preload("res://util/util.gd")
 
+const indicator_lit = preload("res://Assets/Characters/indicator_lit.tres")
+const indicator_unlit = preload("res://Assets/Characters/indicator_unlit.tres")
+
 export var velocity_max_angular = 2
 export var velocity_max_linear = 30
 export var velocity_linear_acceleration = 20
-var player_model: Spatial
+export var charge: float = 0.5
 
-var charge: float = 0.5
+var player_model: Spatial
+var body_mesh: Mesh
+var indicator_lookup = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player_model = self.get_node('PlayerModel') as Spatial
+	
+	# Lookup table for charge indicator surface indices for materials
+	body_mesh = (player_model.get_node("Char1_Body/Body2") as MeshInstance).mesh
+	for i in range(0, body_mesh.get_surface_count()):
+		print(body_mesh.surface_get_material(i).resource_name)
+		match body_mesh.surface_get_material(i).resource_name:
+			"Charge Indicator":
+				indicator_lookup[0] = i
+			"Charge Indicator 2":
+				indicator_lookup[1] = i
+			"Charge Indicator 3":
+				indicator_lookup[2] = i
+			"Charge Indicator 4":
+				indicator_lookup[3] = i
+			"Charge Indicator 5":
+				indicator_lookup[4] = i
+			"Charge indicator 6":
+				indicator_lookup[5] = i
 
 func _physics_process(delta):
 	var velocity_abs = self.linear_velocity.length()
@@ -35,6 +58,11 @@ func _physics_process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	util.display(self, "charge %f" % charge)
+	for i in range(0, 6):
+		if (charge >= (i + 0.5) / 6.0):
+			body_mesh.surface_set_material(indicator_lookup[i], indicator_lit)
+		else:
+			body_mesh.surface_set_material(indicator_lookup[i], indicator_unlit)
 
 func add_charge(amount: float):
 	charge += amount
