2c9d62c14d9e9dd636ade1937cd79c9f3130a424
outfrost <kotlet.bahn@gmail.com>
2020-07-25 07:03:09 +0200

    Refactor ChargingStation, move MovementTest into its little corner

--- functional/ChargingStation.gd
+++ functional/ChargingStation.gd
@@ -4,9 +4,10 @@ const Robot = preload("res://functional/Robot.gd")
 
 export var max_charging_speed: float = 0.05
 
-var material: SpatialMaterial;
+var material: SpatialMaterial; # debug shit
 
-var docked_robot: Robot;
+var robot: Robot
+var docked: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,22 +15,23 @@ func _ready():
 	if material == null:
 		material = SpatialMaterial.new()
 		(get_node("MeshInstance").mesh as CubeMesh).material = material
+	robot = get_tree().root.find_node("Robot", true, false)
 	connect("body_entered", self, "on_body_entered_dock")
 	connect("body_exited", self, "on_body_exited_dock")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if docked_robot:
-		docked_robot.add_charge(max_charging_speed * delta)
+	if docked:
+		robot.add_charge(max_charging_speed * delta)
 
 func on_body_entered_dock(body):
-	if body is Robot:
-		docked_robot = body
+	if body == robot:
+		docked = true
 	material.albedo_color.g = 0.0
 	material.albedo_color.b = 0.0
 
 func on_body_exited_dock(body):
-	if body == docked_robot:
-		docked_robot = null
+	if body == robot:
+		docked = false
 	material.albedo_color.g = 1.0
 	material.albedo_color.b = 1.0
