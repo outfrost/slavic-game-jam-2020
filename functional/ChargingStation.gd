e28768906c028788f6e8f343bff1402a3930d88e
outfrost <kotlet.bahn@gmail.com>
2020-07-25 11:25:24 +0200

    Replace charger model, rescale player

--- functional/ChargingStation.gd
+++ functional/ChargingStation.gd
@@ -6,17 +6,11 @@ const Robot = preload("res://functional/Robot.gd")
 export var max_charging_speed: float = 0.05
 export var charging_falloff_distance: float = 10.0
 
-var material: SpatialMaterial; # debug shit
-
 var robot: Robot
 var docked: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	material = (get_node("MeshInstance").mesh as CubeMesh).material
-	if !material:
-		material = SpatialMaterial.new()
-		(get_node("MeshInstance").mesh as CubeMesh).material = material
 	robot = get_tree().root.find_node("Robot", true, false)
 	if !robot:
 		printerr("Charging station lost connection to robot :(")
@@ -37,11 +31,7 @@ func _process(delta):
 func on_body_entered_dock(body):
 	if body == robot:
 		docked = true
-	material.albedo_color.g = 0.0
-	material.albedo_color.b = 0.0
 
 func on_body_exited_dock(body):
 	if body == robot:
 		docked = false
-	material.albedo_color.g = 1.0
-	material.albedo_color.b = 1.0
